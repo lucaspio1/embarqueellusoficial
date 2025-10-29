960da0526b1af67bcf7b27bd6a033adbbd250ff4
lucas <lucaspioh@gmail.com>
2025-10-29 20:08:53 -0300

    atualizações para login script

--- lib/services/alunos_sync_service.dart
+++ lib/services/alunos_sync_service.dart
@@ -10,7 +10,7 @@ class AlunosSyncService {
   final _db = DatabaseHelper.instance;
 
   final String _apiUrl =
-      'https://script.google.com/macros/s/AKfycbwb9dYG8PrcEnWxLUHEoGxOgpKd5NFqN8QAaluNJ8DLKWrdBssKdK2VfwHSAW6gDLhf/exec';
+      'https://script.google.com/macros/s/AKfycbwDBG1HvpGk7ljWLfl2IxuxttpmDxTMB0RUBwIrQkj_qDUkRar8JGbkvEnapoDLpfO5/exec';
 
   /// Sincroniza PESSOAS da aba PESSOAS do Google Sheets (todos com embeddings)
   Future<SyncResult> syncPessoasFromSheets() async {
--- lib/services/offline_sync_service.dart
+++ lib/services/offline_sync_service.dart
@@ -17,7 +17,7 @@ class OfflineSyncService {
   OfflineSyncService._();
   static final OfflineSyncService instance = OfflineSyncService._();
 
-  final String _sheetsWebhook = 'https://script.google.com/macros/s/AKfycbwb9dYG8PrcEnWxLUHEoGxOgpKd5NFqN8QAaluNJ8DLKWrdBssKdK2VfwHSAW6gDLhf/exec';
+  final String _sheetsWebhook = 'https://script.google.com/macros/s/AKfycbwDBG1HvpGk7ljWLfl2IxuxttpmDxTMB0RUBwIrQkj_qDUkRar8JGbkvEnapoDLpfO5/exec';
   final DatabaseHelper _db = DatabaseHelper.instance;
 
   Timer? _syncTimer;
